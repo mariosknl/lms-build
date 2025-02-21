85c131646c21fc7fe0e5937f9dce4fe22b418075
mariosknl <mariosknl@outlook.com>
2025-02-21 17:34:53 +0200

    implementing sanity presentation feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { draftMode } from "next/headers";
+import { VisualEditing } from "next-sanity";
+import { DisableDraftMode } from "@/components/DisableDraftMode";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +21,7 @@ export const metadata: Metadata = {
   description: "LMS Build",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -28,6 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {(await draftMode()).isEnabled && (
+          <>
+            <DisableDraftMode />
+            <VisualEditing />
+          </>
+        )}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- sanity/lib/adminClient.ts
+++ sanity/lib/adminClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "next-sanity";
 
 import { apiVersion, dataset, projectId } from "../env";
+import baseUrl from "@/lib/baseUrl";
 
 export const client = createClient({
   projectId,
@@ -8,4 +9,7 @@ export const client = createClient({
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
   token: process.env.SANITY_API_ADMIN_TOKEN,
+  stega: {
+    studioUrl: `${baseUrl}/studio`,
+  },
 });
